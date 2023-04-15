edcc1c80369429fb6808aff442b14d3c7a6fd8c3
Harshil Pradhan <harshilpradhan@simformsolutions.com>
2023-04-15 17:46:27 +0530

    add payment gateway

--- src/pages/ActiveUsers.tsx
+++ src/pages/ActiveUsers.tsx
@@ -1,27 +1,22 @@
-import { useEffect } from 'react';
-import { Button } from '@mantine/core';
-import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { Button } from "@mantine/core";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const ActiveUsers = () => {
   const client = useQueryClient();
   const { data = [] } = useQuery({
-    queryKey: ['user'],
+    queryKey: ["user"],
     queryFn: () =>
-      fetch('https://jsonplaceholder.typicode.com/users').then((e) =>
-        e.json()
-      ) as Promise<any[]>,
+      fetch("https://jsonplaceholder.typicode.com/users").then((e) => e.json()) as Promise<any[]>,
 
     // Side effects
-    onSuccess: (data) => console.log('success'),
-    onError: () => console.log('Error'),
-    onSettled: () => console.log('Finished'),
+    onSuccess: (data) => console.log("success"),
+    onError: () => console.log("Error"),
+    onSettled: () => console.log("Finished"),
   });
 
   return (
     <>
-      <Button onClick={() => client.invalidateQueries(['users'])}>
-        Refresh
-      </Button>
+      <Button onClick={() => client.invalidateQueries(["users"])}>Refresh</Button>
       {data.map((item) => (
         <p key={item.id}>{item.name}</p>
       ))}
